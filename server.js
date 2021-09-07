53f81fcae04cdf1f2acb731709fb30c5d04997ff
Aimee <aimeeharkin@gmail.com>
2021-09-07 09:59:14 +0200

    configured mongoose to establish connection to database and tested with a get request

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const port = 5000;
 import paintingRouter from './routes/paintingRouter.js';
+
+// MONGOOSE CONFIG
+mongoose.connect(`mongodb+srv://aimhark:aimhark@cluster0.0cmhp.mongodb.net/paintings_db?retryWrites=true&w=majority`,
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+.then(()=> console.log(`Connection to Database established`))
+.catch((err) => console.log(`Unable to connect to the Database`))
 
 // MIDDLEWARE
 app.use(express.json());
