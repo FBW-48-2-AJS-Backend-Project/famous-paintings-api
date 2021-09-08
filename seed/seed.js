78a72f9ae49ea8c75287095c01c80143d07243c4
Aimee <aimeeharkin@gmail.com>
2021-09-08 15:44:17 +0200

    removed faker database in seed.js  and populated database in insomnia with real paintings

--- seed/seed.js
+++ seed/seed.js
@@ -16,31 +16,31 @@ import faker from 'faker';
     .catch((err) => console.log(`Unable to establish connection to Database`))
 
     // CREATE 20 FAKE PAINTINGS
-    const paintingsPromise = Array(20)
-    .fill(null)
-    .map(()=> {
-        const paintingData = {
-            title: faker.random.words(),
-            artist: faker.random.word()+" "+faker.random.word(),
-            year: faker.date.past(),
-            price: faker.commerce.price(10,20),
-            cover: faker.image.image(400, 400),
-        };
-        console.log(`Painting ${paintingData.title} from ${paintingData.artist} has been created`);
+    // const paintingsPromise = Array(20)
+    // .fill(null)
+    // .map(()=> {
+    //     const paintingData = {
+    //         title: faker.random.words(),
+    //         artist: faker.random.word()+" "+faker.random.word(),
+    //         year: faker.date.past(),
+    //         price: faker.commerce.price(10,20),
+    //         cover: faker.image.image(400, 400),
+    //     };
+    //     console.log(`Painting ${paintingData.title} from ${paintingData.artist} has been created`);
 
-        const newPainting = new Painting(paintingData);
-        return newPainting.save();
-    })
+    //     const newPainting = new Painting(paintingData);
+    //     return newPainting.save();
+    // })
 
-    try{
-        await Promise.all(paintingsPromise);
-        console.log('-----------------------------------------');
-        console.log(`All 20 fake paintings have been stored on the database`);
-        console.log('-----------------------------------------');
-    }
-    catch(err){
-        console.log(err);
-    };
+    // try{
+    //     await Promise.all(paintingsPromise);
+    //     console.log('-----------------------------------------');
+    //     console.log(`All 20 fake paintings have been stored on the database`);
+    //     console.log('-----------------------------------------');
+    // }
+    // catch(err){
+    //     console.log(err);
+    // };
 
     // CLOSE CONNECTION TO DB
     mongoose.connection.close();
